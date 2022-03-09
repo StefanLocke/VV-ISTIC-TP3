b20028839650a83c5d103a4b44b9f5df3e5a16f0
Martin <m.thomas247@laposte.net>
2022-03-09 18:11:36 +0100

    TP3 exercice 3 : IsBalenced algorithme implémentation. Tests semblent passer. Compléter le réponses dans le .md

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,39 @@
 package fr.istic.vv;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class StringUtils {
 
-    private StringUtils() {}
+    private Map<Character,Character> characterMap;
 
-    public static boolean isBalanced(String str) {
-        return false;
+    public StringUtils() {
+        this.characterMap= new HashMap<Character,Character>();
+    }
+
+    private boolean isOpeningSymbol(char c){
+        return this.characterMap.containsKey(c);
+    }
+
+    private boolean isClosingSymbol(char c){
+        return this.characterMap.containsValue(c);
+    }
+
+    private boolean isCorrespondingSymbol(char open,char close){
+        return (characterMap.containsKey(open) &&characterMap.get(open).equals(close) );
+    }
+
+    public boolean isBalanced(String str){
+        LinkedList<Character> queue = new LinkedList<Character>();
+        for (Character c : str.toCharArray()){
+            if(isOpeningSymbol(c)){
+                queue.addLast(c);
+            }else if(isClosingSymbol(c) && !queue.isEmpty() && isCorrespondingSymbol(queue.getLast(),c)){
+                queue.removeLast();
+            }
+        }
+        return queue.isEmpty();
     }
 
 }
--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -1,11 +1,41 @@
 package fr.istic.vv;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static fr.istic.vv.StringUtils.isBalanced;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+    StringUtils stringUtils;
 
+    @BeforeEach
+    void init(){
+        stringUtils= new StringUtils();
+    }
+
+    @Test
+    void isBalancedTest_0(){
+        assertTrue(stringUtils.isBalanced("{[][]}({})"));
+    }
+
+    @Test
+    void isBalancedTest_1(){
+        assertFalse(stringUtils.isBalanced("{"));
+    }
+
+    @Test
+    void isBalancedTest_2(){
+        assertFalse(stringUtils.isBalanced("{([)]"));
+    }
+
+    @Test
+    void isBalancedTest_3(){
+        assertFalse(stringUtils.isBalanced("{(}{}"));
+    }
+
+    @Test
+    void isBalancedTest_4(){
+        assertFalse(stringUtils.isBalanced("]["));
+    }
 }
