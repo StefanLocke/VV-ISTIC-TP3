ddf9a73fc40c58826bb301541c99b539d6240393
Martin Thomas <martin.thomas.1@etudiant.univ-rennes1.fr>
2022-03-10 15:16:03 +0100

    isBalanded main program - interractive

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,5 +1,7 @@
 package fr.istic.vv;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
@@ -9,7 +11,20 @@ public class StringUtils {
     private Map<Character,Character> characterMap;
 
     public StringUtils() {
+
         this.characterMap= new HashMap<Character,Character>();
+    }
+
+    public void setCharacterMap(Map<Character, Character> characterMap) {
+        this.characterMap = characterMap;
+    }
+
+    public void  initFromStrings(String[] array) throws WrongInitialSetException, EmptyInitialSetException {
+        if (array.length <= 0) throw new EmptyInitialSetException();
+        for( String entry : array){
+            if(entry.length() != 2) throw new WrongInitialSetException();
+            this.characterMap.put(entry.toCharArray()[0],entry.toCharArray()[1]);
+        }
     }
 
     private boolean isOpeningSymbol(char c){
@@ -24,16 +39,40 @@ public class StringUtils {
         return (characterMap.containsKey(open) &&characterMap.get(open).equals(close) );
     }
 
-    public boolean isBalanced(String str){
+    public boolean isBalanced(String str) throws EmptyInitialSetException, SymbolNotInSetException {
+        if (this.characterMap.isEmpty()) throw new EmptyInitialSetException();
         LinkedList<Character> queue = new LinkedList<Character>();
         for (Character c : str.toCharArray()){
             if(isOpeningSymbol(c)){
                 queue.addLast(c);
             }else if(isClosingSymbol(c) && !queue.isEmpty() && isCorrespondingSymbol(queue.getLast(),c)){
                 queue.removeLast();
-            }
+            }else if (!isOpeningSymbol(c) && !isClosingSymbol(c)) throw new SymbolNotInSetException(c);
         }
         return queue.isEmpty();
+    }
+
+    public class EmptyInitialSetException extends Exception {
+        @Override
+        public String getMessage() {
+            return "Initial HashMap of opening and closing symbol is empty.";
+        }
+    }
+
+    @RequiredArgsConstructor
+    public class SymbolNotInSetException extends Exception {
+        private final char c;
+        @Override
+        public String getMessage() {
+            return "Given symbol '"+c+"' is not in symbol table.";
+        }
+    }
+
+    public class WrongInitialSetException extends Exception {
+        @Override
+        public String getMessage() {
+            return "Given initial set is not properly formatted - cannot map symbols.";
+        }
     }
 
 }
--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -3,6 +3,8 @@ package fr.istic.vv;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
@@ -11,31 +13,37 @@ class StringUtilsTest {
 
     @BeforeEach
     void init(){
+
         stringUtils= new StringUtils();
+        HashMap<Character,Character> set = new HashMap<Character,Character>();
+        set.put('(',')');
+        set.put('{','}');
+        set.put('[',']');
+        stringUtils.setCharacterMap(set);
     }
 
     @Test
-    void isBalancedTest_0(){
+    void isBalancedTest_0() throws StringUtils.EmptyInitialSetException, StringUtils.SymbolNotInSetException {
         assertTrue(stringUtils.isBalanced("{[][]}({})"));
     }
 
     @Test
-    void isBalancedTest_1(){
+    void isBalancedTest_1() throws StringUtils.EmptyInitialSetException, StringUtils.SymbolNotInSetException {
         assertFalse(stringUtils.isBalanced("{"));
     }
 
     @Test
-    void isBalancedTest_2(){
+    void isBalancedTest_2() throws StringUtils.EmptyInitialSetException, StringUtils.SymbolNotInSetException {
         assertFalse(stringUtils.isBalanced("{([)]"));
     }
 
     @Test
-    void isBalancedTest_3(){
+    void isBalancedTest_3() throws StringUtils.EmptyInitialSetException, StringUtils.SymbolNotInSetException {
         assertFalse(stringUtils.isBalanced("{(}{}"));
     }
 
     @Test
-    void isBalancedTest_4(){
+    void isBalancedTest_4() throws StringUtils.EmptyInitialSetException, StringUtils.SymbolNotInSetException {
         assertFalse(stringUtils.isBalanced("]["));
     }
 }
