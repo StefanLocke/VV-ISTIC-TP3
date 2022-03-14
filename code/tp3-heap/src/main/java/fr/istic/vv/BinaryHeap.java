657a1a1695d9407bfe1fe216bef3030fd8fb7c49
Martin Thomas <martin.thomas.1@etudiant.univ-rennes1.fr>
2022-03-14 19:45:34 +0100

    WIP TP3.5

--- code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
+++ code/tp3-heap/src/main/java/fr/istic/vv/BinaryHeap.java
@@ -1,17 +1,50 @@
 package fr.istic.vv;
 
+import lombok.RequiredArgsConstructor;
+
 import java.util.Comparator;
 
-class BinaryHeap<T> {
+class BinaryHeap<T,NotSuchElementException extends Throwable> {
 
-    public BinaryHeap(Comparator<T> comparator) {  }
+    T value;
+    BinaryHeap<T,NotSuchElementException> leftChild;
+    BinaryHeap<T,NotSuchElementException> rightChild;
 
-    public T pop() { return null; }
+    Comparator<T> comparator;
 
-    public T peek() { return null; }
+    public BinaryHeap(Comparator<T> comparator) {
+        this.comparator = comparator;
+    }
 
-    public void push(T element) {  }
+    public T pop() {
+        // TODO
+        return value;
+    }
 
-    public int count() { return 0; }
+    public T peek() {
+        // TODO
+        return value;
+    }
+
+
+    public void push(T element)  {
+        if (value==null) this.value = element;
+        switch (comparator.compare(value,element)){
+            case 0 : this.value = element; break;
+            case -1:
+                if (this.leftChild==null) this.leftChild = new BinaryHeap<T,NotSuchElementException>(comparator);
+                this.leftChild.push(element);
+                break;
+            case 1 :
+                if (this.rightChild==null) this.rightChild = new BinaryHeap<T,NotSuchElementException>(comparator);
+                this.rightChild.push(element);
+                break;
+        }
+    }
+
+    public int count(){
+        // TODO
+        return 0;
+    }
 
 }
